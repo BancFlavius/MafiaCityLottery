36b2810f44473606bdbf042debf2bf92a335500e
Flavius <bancflavius@gmail.com>
2020-07-11 17:46:20 +0300

    update LoginActivity remove redundant method and called for Validator class method update activity login and signup layout add progress bar and image view with app logo

--- app/src/main/java/com/mafiacity/lottery/LoginActivity.java
+++ app/src/main/java/com/mafiacity/lottery/LoginActivity.java
@@ -2,7 +2,6 @@ package com.mafiacity.lottery;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -11,13 +10,12 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private TextInputEditText emailView, passwordView;
 
     private Button btnLogin;
 
@@ -34,9 +32,6 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void initializeViews(){
-        emailView = findViewById(R.id.et_email);
-        passwordView = findViewById(R.id.et_password);
-
         btnLogin = findViewById(R.id.btn_login);
         btnLogin.setOnClickListener(this::onClick);
     }
@@ -44,14 +39,14 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-//
-//        FirebaseUser currentUser = mAuth.getCurrentUser();
-//
-//        updateUi(currentUser);
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        updateUi(currentUser);
     }
 
     private void updateUi(FirebaseUser user) {
-        if(user !=null){
+        if(!user.getDisplayName().isEmpty()){
             LoginActivity.this.startActivity(new Intent(LoginActivity.this, MainMenuActivity.class));
         }
 
@@ -62,47 +57,28 @@ public class LoginActivity extends AppCompatActivity {
         updateUi(null);
     }
 
-    private void signIn(String email, String password) {
+    private void signIn() {
 
-        if (!validateForm()) {
-            return;
+        TextInputLayout emailView = findViewById(R.id.et_email_layout);
+        TextInputLayout passwordView = findViewById(R.id.et_password_layout);
+
+        String[] credentials = Validator.validateLogin(emailView, passwordView);
+        if( credentials != null ) {
+
+            mAuth.signInWithEmailAndPassword(credentials[0], credentials[1])
+                    .addOnCompleteListener(this, task -> {
+                        if (task.isSuccessful()) {
+                            Log.d("INFO", "successful sign in");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUi(user);
+                        } else {
+                            Log.d("INFO", "Unsuccessful sign in");
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUi(null);
+                        }
+                    });
         }
-
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, task -> {
-                    if (task.isSuccessful()) {
-                        Log.d("INFO", "successful sign in");
-                        FirebaseUser user = mAuth.getCurrentUser();
-                        updateUi(user);
-                    } else {
-                        Log.d("INFO", "Unsuccessful sign in");
-                        Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                Toast.LENGTH_SHORT).show();
-                        updateUi(null);
-                    }
-                });
-    }
-
-    private boolean validateForm() {
-        boolean valid = true;
-
-        String email = emailView.getText().toString();
-        if (TextUtils.isEmpty(email)) {
-            emailView.setError("Required.");
-            valid = false;
-        } else {
-            emailView.setError(null);
-        }
-
-        String password = passwordView.getText().toString();
-        if (TextUtils.isEmpty(password)) {
-            passwordView.setError("Required.");
-            valid = false;
-        } else {
-            passwordView.setError(null);
-        }
-
-        return valid;
     }
 
     public void onClick(View view) {
@@ -113,8 +89,9 @@ public class LoginActivity extends AppCompatActivity {
                 break;
             case R.id.btn_login:
                 Log.d("INFO", "User log in");
-                signIn(emailView.getText().toString(), passwordView.getText().toString());
+                signIn();
                 break;
         }
     }
+
 }
--- app/src/main/java/com/mafiacity/lottery/SignUpActivity.java
+++ app/src/main/java/com/mafiacity/lottery/SignUpActivity.java
@@ -109,5 +109,4 @@ public class SignUpActivity extends AppCompatActivity {
                 break;
         }
     }
-
 }
